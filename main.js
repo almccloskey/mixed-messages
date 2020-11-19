6846aa0da1413b9573eab0662e4e677205141d9d
[almccloskey@me.com] <60784483+almccloskey@users.noreply.github.com>
2020-11-19 17:10:37 -0600

    first commit of initial construct

---
+++ main.js
@@ -0,0 +1,22 @@
+function randomIndex (arr) {
+    let item = arr[Math.floor(Math.random() * (arr.length - 1))];
+    return item
+}
+
+const firstPhrase = ['You should ', 'You are ', 'Your life '];
+
+const shouldPhrase = ['trust ', 'go ', 'stop '];
+    const trustPhrase = ['no one.', 'yourself!', 'the wind...'];
+    const goPhrase = ['to sleep. Goodnight!', 'for a walk!', 'back to school?'];
+    const stopPhrase = ['listening to you inner demons.', 'giving yourself such a hard time!', 'for a second and smell the flowers...'];
+
+const arePhrase = ['having ', 'a ', 'doing ']
+    const havingPhrase = ['a good day.', 'a terrible time right now, sorry!', 'a baby. Quick, call 911!']
+    const 
+
+
+
+function generateMessage () {
+}
+
+console.log('this is a test')
